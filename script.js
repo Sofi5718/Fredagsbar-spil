ab39b527bceab74e8e04c0ad2bc6cea0fa1e0f81
Sofie Larsen <Sofieamalierl@hotmail.com>
2023-02-21 12:03:49 +0100

    Få og miste liv

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 window.addEventListener("load", start);
 
 let points = 0;
+let lives = 3;
 
 function start() {
   console.log("JavaScript kører!");
@@ -11,12 +12,11 @@ function start() {
   document.querySelector("#bomb_container").classList.add("falling");
 
   // Registrer click
-  document
-    .querySelector("#coin1_container")
-    .addEventListener("click", clickCoin);
-  document
-    .querySelector("#bomb_container")
-    .addEventListener("click", clickBomb);
+  document.querySelector("#coin1_container").addEventListener("click", clickCoin);
+  document.querySelector("#bomb_container").addEventListener("click", clickBomb);
+
+  document.querySelector("#heart_container").addEventListener("click", clickHeart);
+
 }
 
 function clickCoin() {
@@ -36,6 +36,7 @@ function clickCoin() {
   document.querySelector("#coin1_container").addEventListener("animationend", coinGone);
   incrementPoints();
 }
+
 //Funktioner vi laver som øvelse i timen
 function incrementPoints() {
   points += 1;
@@ -81,10 +82,23 @@ function clickBomb() {
   document.querySelector("#bomb_sprite").classList.add("zoom_in");
 
   // når forsvind-animation er færdig: coinGone
-  document
-    .querySelector("#bomb_container")
-    .addEventListener("animationend", bombGone);
+  document.querySelector("#bomb_container").addEventListener("animationend", bombGone);
+  //Mist liv i korrekt rækkefælge, ved at klikke på bombe
+  decrementLives();
 }
+function decrementLives() {
+  console.log("decrementLives");
+  displayDecrementedLives();
+  lives -=1; 
+  console.log(lives);
+}
+function displayDecrementedLives() {
+  document.querySelector("#heart" + lives).classList.remove("active_heart");
+  document.querySelector("#heart" + lives).classList.add("broken_heart");
+  
+}
+
+  //Mist liv i korrekt rækkefælge, ved at klikke på bombe
 
 function bombGone() {
   // fjern event der bringer os herind
@@ -105,4 +119,47 @@ function bombGone() {
   document
     .querySelector("#bomb_container")
     .addEventListener("click", clickBomb);
+}
+function clickHeart() {
+  console.log("click");
+   document.querySelector("#bomb_container").removeEventListener("click", clickHeart);
+
+   // Stop coin container
+   document.querySelector("#heart_container").classList.add("paused");
+
+   // sæt forsvind-animation på coin
+   document.querySelector("#heart_sprite").classList.add("zoom_in");
+
+   // når forsvind-animation er færdig: coinGone
+  document.querySelector("#heart_container").addEventListener("animationend", heartGone);
+  incrementLives();
+}
+function heartGone() {
+  // fjern event der bringer os herind
+  document.querySelector("#heart_container").removeEventListener("animationend", heartGone);
+
+  // fjern forsvind-animation
+  document.querySelector("#heart_sprite").classList.remove("zoom_in");
+
+  // fjern pause
+  document.querySelector("#heart_container").classList.remove("paused");
+
+  // genstart falling animation
+  document.querySelector("#heart_container").classList.remove("falling");
+  document.querySelector("#heart_container").offsetWidth;
+  document.querySelector("#heart_container").classList.add("falling");
+
+  // gør det muligt at klikke på coin igen
+  document
+    .querySelector("#heart_container")
+    .addEventListener("click", clickHeart);
+}
+function incrementLives() {
+  lives += 1;
+  displayincrementLives();
+  
+}
+function displayincrementLives() {
+  document.querySelector("#heart" + lives).classList.remove("broken_heart");
+  document.querySelector("#heart" + lives).classList.add("active_heart");
 }
